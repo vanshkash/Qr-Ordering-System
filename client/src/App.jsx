a01e5228ffa55f26710b47abc438ef11a515ff59
Vanshkash <vkexpert87@gmail.com>
2026-02-27 02:41:40 +0530

    Added default redirect route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Menu from "./pages/Menu";
 import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import TrackOrder from "./pages/TrackOrder";
 function App() {
 
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to="/menu/1" />} />
       <Route path="/menu/:tableId" element={<Menu />} />
       <Route path="/track/:orderId" element={<TrackOrder />} />
     </Routes>
